334bb4e42db1b1035e26a59bf184853bae404a15
EC2 Default User <ec2-user@ip-172-31-30-129.eu-west-1.compute.internal>
2023-01-04 20:06:39 +0000

    test pipelines

--- tasks.py
+++ tasks.py
@@ -37,16 +37,16 @@ def stack_build(ctx):
 
 
 @task(pre=[stack_build])
-def stack_deploy(ctx, cicd_bucket, stage):
+def stack_deploy(ctx):
     with ctx.prefix(_activate()):
-        ctx.run(f"aws s3 cp spec/api-spec.yaml s3://{cicd_bucket}/spec/api-spec.yaml")
-        ctx.run(f"sam package --s3-bucket {cicd_bucket} --output-template-file packaged.yaml")
+        ctx.run(f"aws s3 cp spec/api-spec.yaml s3://s3-eu-west-1-cicd-awesomecs/spec/api-spec.yaml")
+        ctx.run(f"sam package --s3-bucket s3-eu-west-1-cicd-awesomecs --output-template-file packaged.yaml")
         ctx.run(f"sam deploy packaged.yaml "
                 f"--capabilities CAPABILITY_IAM "
                 f"--parameter-overrides "
-                f"StageName={stage} CicdBucket={cicd_bucket} "
-                f"--s3-bucket {cicd_bucket} "
-                f"--stack-name {_project_name()}-{stage} "
+                f"StageName=awesomecms CicdBucket=s3-eu-west-1-cicd-awesomecs "
+                f"--s3-bucket s3-eu-west-1-cicd-awesomecs "
+                f"--stack-name {_project_name()}-awesomecms "
                 f"--region {_aws_region()} ")
 
 
